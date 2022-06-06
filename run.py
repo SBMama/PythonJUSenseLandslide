ec5fdd130f3d570e38217b9b358f7539b5e31f7f
SBMama <b.subhayan@yahoo.com>
2022-06-06 12:00:07 +0530

    trying to resolve relative filepath

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ CORS(app)
 
 @app.route('/v1/get_tweets', methods=['GET'])
 def get_tweets():
-    filename = os.path.abspath(os.path.join("..","data","tweets.json"))
+    filename = os.path.abspath("../data/tweets.json")
     params = request.args.to_dict()
     tweet_obj = ProcessTweet(filename)
     result = tweet_obj.map_tweets_from_json()
